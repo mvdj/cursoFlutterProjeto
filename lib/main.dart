c462766fc3ae3aa0f66dc597a702ddb90173be26
mvdj <mauriciodias.junior@gmail.com>
2021-10-31 11:53:39 -0300

    camada de domínio

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,31 @@
 //@dart=2.9
+import 'package:farmacia/app/injection.dart';
 import 'package:farmacia/app/my_app.dart';
 import 'package:flutter/material.dart';
-
+  
 void main() {
+  setupinjection();
   runApp(const MyApp());
 }
 
 
-/* ATIVIDADE
-(1) três exemplos de funções assíncronas e um texto explicativo do que é future, await e async 
-e para que serve a programação assíncrona; e (2) a definição do script do BD (está no arquivo script.dart)
+/* ATIVIDADE  
 
+Envie o link do GitHub do projeto com o commit intitulada como "camada de domínio" que possua: 
+(1) injeção de dependência;  OK
+(2) camada de domínio; OK
+(3) um texto explicativo do objetivo da injeção de dependência e da camada de domínio.
 
-A classe "Future" funciona como algo (como exemplo: um método) poderá ser executado no futuro determinado por um tempo.
-Já a palavra reservada "async" determina que terá uma função dentro do contexto que se encontra um método futuro sinalizado 
-com outra palavra reservada denominada "await" na qual irá esperar este método executar por completo para poder seguir 
-na execução dos comandos posteriores.
+A injeção de dependẽncia foi desenvolvida, com o padrão de projeto Singleton para ficar independente do DAO, 
+sendo executado apenas uma vez por requisição, otimizando o tempo de resposta.
 
-Enquanto que a programação sequencial sincronizada tem a necessidade de aguardar TODOS os comandos 
-serem executados na sequência (desperdiçando tempo e recursos computacionais) parando o programa até que 
-seja executados todos, na programação assíncrona pode ser executados comandos posteriores, independentemente do 
-tempo a ser esperado dos comandos anteriores, ocasionando uma dinâmica mais interessante para diversos contextos.*/
-
-/*
-void main() async{
-  print("Carregando tela, logotipo e descrições...");
-  tela();
-  logotipos();
-  await descricoesRemedios();
+setupinjection(){
+  GetIt getit = GetIt.I;
+  getit.registerSingleton<RemedioDAO>(RemedioDAOImpl());
 }
 
-Future tela(){
-  return Future.delayed(Duration(seconds: 5),() => print("Tela carregada!"));
-}
+A camada de domínio foi desenvolvida com as entidades (Remedio), as exceções,  interfaces e services, 
+sendo independente do DAO.
 
-Future logotipos(){
-  return Future.delayed(Duration(seconds: 5),() => print("Logotipos carregados!"));
-}
-
-Future descricoesRemedios(){
-  return Future.delayed(Duration(seconds: 5),() => print("Busca no BD das descrições realizadas."));
-}
 
 */
